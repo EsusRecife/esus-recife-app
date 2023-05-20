fdd85beb495f88d8f7a4c4a43f453ebf2a400a6c
Felipe Pereira <felipe.pereira.nave@gmail.com>
2023-05-20 20:40:48 -0300

    feat: *Add all html into Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,1 +1,19 @@
+// import logo from '../assets/logo-esus.svg';
 import "./styles/logo.css"
+
+export function Logo() {
+    return(
+        <>
+            <div className='welcomegroup2'>
+
+                <div className='esus-logo-name'>
+                    <div>
+                        {/* <img src={logo} alt="Logo-Esus" /> */}
+                    </div>
+                    <p>Esus Recife</p>
+                </div>
+                
+            </div>
+        </>
+    )
+}
