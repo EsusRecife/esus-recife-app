0d45109b96bf4bfae881afec12fc14e7efc78df4
Felipe Pereira <felipe.pereira.nave@gmail.com>
2023-05-20 20:42:58 -0300

    feat: Add all the html into FirstLayer.tsx

--- src/components/Firstlayer.tsx
+++ src/components/Firstlayer.tsx
@@ -1,1 +1,57 @@
 import './styles/firstlayer.css'
+import {FormLogin} from './FormLogin';
+import { FormSenha } from './FormLogin';
+import WelcomeMessage from './WelcomeMessage';
+import { FormRegister } from './FormLogin';
+import { Logo } from './Logo';
+
+export function LoginSplash() {
+    return(
+        <>
+            <div className='firstlayer'>
+                
+                <div className='clipcircle'>
+                    <WelcomeMessage></WelcomeMessage>
+                </div>
+
+                <div className='formlogin'>
+                    <FormLogin></FormLogin>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export function SenhaSplash() {
+    return(
+        <>
+            <div className='firstlayer'>
+                
+                <div className='clipcircle'>
+                    <Logo></Logo>
+                </div>
+
+                <div className='formlogin'>
+                    <FormSenha></FormSenha>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export function RegisterSplash() {
+    return(
+        <>
+            <div className='firstlayer'>
+
+                <div >
+                    <FormRegister></FormRegister>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
